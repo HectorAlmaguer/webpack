dff9edaee541f601790ccc2c856835ddfbff0c31
Hector <hectorm.almaguerc@gmail.com>
2021-10-04 17:43:07 -0500

    Webpack-babel

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import 'fslightbox';
+/* fsLightboxInstances['first-lightbox'].open(0);
+fsLightboxInstances['second-lightbox'].props.onOpen = () => console.log('Lightbox open!'); */
+
+/* 
+
+import {suma, saludo} from './operaciones';
+import Swal from 'sweetalert2';
+document.write(`<h1>${suma(1,2)}</h1>`);
+document.write(`<h1>${suma(10,22)}</h1>`);
+console.log(suma(1,2));
+console.log(suma(1,4));
+console.log(saludo);
+
+  Swal.fire({
+    title: 'Hector Almaguer',
+    text: 'Alert con Image personal',
+    imageUrl: 'https://www.spaziotribu.org/wp-content/uploads/2019/06/mario-bros.gif',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
+
+
+import _ from 'lodash';
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component()); */
+
